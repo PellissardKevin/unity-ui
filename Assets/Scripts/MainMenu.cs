6327538198ad37397da8e7c4d6976ec74d14f4b1
PellissardKevin <kevin.pellissard@gmail.com>
2024-05-21 20:04:41 +0200

    Script mainmenu for toggle colorblind or not !

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 255);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = new Color32(255, 0, 0, 255);
+            goalMat.color = new Color32(0, 255, 0, 255);
+        }
+        
         SceneManager.LoadScene("maze");
     }
     public void QuitMaze()
